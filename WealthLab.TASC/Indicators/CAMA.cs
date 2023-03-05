f2fb33ccd9b5e3b9e84ed92e7722fff304b51055
LucidDion <lucid.dion@gmail.com>
2023-03-05 09:55:43 -0500

    add CAMA and Hann

---
+++ WealthLab.TASC/Indicators/CAMA.cs
@@ -0,0 +1,61 @@
+﻿using WealthLab.Core;
+using WealthLab.Indicators;
+
+namespace WealthLab.TASC
+{
+    public class CAMA : IndicatorBase
+    {
+        public override string Name=>"Cong Adaptive Moving Average";
+        public override string Abbreviation => "CAMA";
+        public override string HelpDescription=>"Cong Adaptive Moving Average, based on the article by Scott Cong in the March 2023 issue of Stocks & Commodities magazine.";
+        public override string PaneTag=>"Price";
+        public override WLColor DefaultColor => WLColor.Crimson;
+        public override bool IsSmoother => false;
+        public CAMA()
+        {
+        }
+        public CAMA(BarHistory source, int period = 20)
+        {
+            base.Parameters[0].Value = source;
+            base.Parameters[1].Value = period;
+            this.Populate();
+        }
+
+        //static method
+        public static CAMA Series(BarHistory source, int period)
+        {
+            string key = CacheKey("CAMA", period);
+            if (source.Cache.ContainsKey(key))
+                return (CAMA)source.Cache[key];
+            CAMA cama = new CAMA(source, period);
+            source.Cache[key] = cama;
+            return cama;
+        }
+
+        protected override void GenerateParameters()
+        {
+            base.AddParameter("Source", ParameterType.BarHistory, null);
+            base.AddParameter("Lookback Period", ParameterType.Int32, 20);
+        }
+        public override void Populate()
+        {
+            BarHistory bars = base.Parameters[0].AsBarHistory;
+            int period = base.Parameters[1].AsInt;
+            this.DateTimes = bars.DateTimes;
+            int FirstValidValue = period;
+            if (bars.Count < FirstValidValue)
+                return;
+
+            TimeSeries Result = Highest.Series(bars.High, period) - Lowest.Series(bars.Low, period);
+            TimeSeries Effort = TR.Series(bars).Sum(period);
+            TimeSeries alpha = Result / Effort;
+            TimeSeries source_price = bars.Close;
+            TimeSeries cama = new TimeSeries(DateTimes, 0);
+            for (int i = period; i < DateTimes.Count; i++)
+            {
+                cama[i] = alpha[i] * source_price[i] + (1 - alpha[i]) * cama[i - 1];
+            }
+            Values = cama.Values;
+        }
+    }
+}
---
+++ WealthLab.TASC/Indicators/Hann.cs
@@ -0,0 +1,96 @@
+﻿using WealthLab.Core;
+using WealthLab.Indicators;
+
+namespace WealthLab.TASC
+{
+    public class Hann : IndicatorBase
+    {
+        public override string Name => "Hann filter";
+        public override string Abbreviation => "Hann";
+        public override string HelpDescription => "Hann Windowed Lowpass FIR Filter by Dr. John Ehlers from S&C April 2023 issue.";
+        public override string PaneTag => "Price";
+        public override WLColor DefaultColor => WLColor.Indigo;
+        
+        //it's a smoother
+        public override bool IsSmoother => true;
+
+        public Hann()
+        {
+        }
+        public Hann(TimeSeries source, int period = 14, bool undersample = false, int undersampleBars = 5)
+        {
+            base.Parameters[0].Value = source;
+			base.Parameters[1].Value = period;
+            base.Parameters[2].Value = undersample;
+            base.Parameters[3].Value = undersampleBars;
+            this.Populate();
+        }
+
+        //static method
+        public static Hann Series(TimeSeries source, int period = 14, bool undersample = false, int undersampleBars = 5)
+        {
+            string key = CacheKey("Hann", period, undersample, undersampleBars);
+            if (source.Cache.ContainsKey(key))
+                return (Hann)source.Cache[key];
+            Hann h = new Hann(source, period, undersample, undersampleBars);
+            source.Cache[key] = h;
+            return h;
+        }
+
+        protected override void GenerateParameters()
+        {
+            base.AddParameter("Source", ParameterType.TimeSeries, PriceComponent.Close); 
+            base.AddParameter("Lookback Period", ParameterType.Int32, 14);
+            base.AddParameter("Undersample", ParameterType.Boolean, false);
+            base.AddParameter("Undersample bars", ParameterType.Int32, 5);
+        }
+
+        public override void Populate()
+        {
+            TimeSeries source = base.Parameters[0].AsTimeSeries;
+            int period = base.Parameters[1].AsInt;
+            bool undersample = base.Parameters[2].AsBoolean;
+            int underSampleBars = base.Parameters[3].AsInt;
+
+            this.DateTimes = source.DateTimes;
+            int FirstValidValue = period;
+            if (source.Count < FirstValidValue)
+            {
+                return;
+            }
+
+            if (undersample)
+            {
+                TimeSeries _ts = new TimeSeries(source.DateTimes, 0);
+                for (int i = 0; i < _ts.Count; i++)
+                {
+                    if (i % underSampleBars == 0)
+                        _ts[i] = source[i];
+                    else
+                        if (i > 0)
+					_ts[i] = source[i - 1];
+                }
+                source = _ts;                
+            }
+
+            for (int bar = 0; bar < source.Count; bar++)
+            {
+                double Filt = 0, Coef = 0;
+
+                if (bar > period)
+                {
+                    for (int count = 1; count <= period; count++)
+                    {
+                        double ang = 360 * count / (period + 1.0);
+                        double c = 1.0 - Math.Cos(ang.ToRadians());
+                        Filt += c * source[bar - count - 1];
+                        Coef += c;
+                    }
+                }
+
+                if ((Filt + Coef != 0))
+                    base.Values[bar] = Filt / Coef;
+            }
+        }
+    }
+}
